ff2eb686dcc66189b3ace736016e6d5dc757719c
nana <sss1230871997@gmail.com>
2024-12-01 20:01:30 +0900

    default_url_option product

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,9 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+# デフォルトURLオプションを設定
+config.action_mailer.default_url_options = { host: 'maruichicafe.herokuapp.com', protocol: 'https' }
+Rails.application.routes.default_url_options[:host] = 'maruichicafe.herokuapp.com'
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
